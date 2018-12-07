0149c1ada0c6913fc4f16ab858c233c7a5bc94b3
Marko Dogramadzi <md15006@my.bristol.ac.uk>
2018-12-07 17:12:09 +0000

    Database not connecting

--- app/app.py
+++ app/app.py
@@ -1,62 +1,31 @@
 from typing import List, Dict
 from flask import Flask, render_template, request
-<<<<<<< HEAD
-from flask.ext.mysql import MySQL 
-=======
-import mysql.connector
->>>>>>> 4443eb0c81d88fd25681c3449a841fe74e0dad89
+import pymysql
 import json
+
 
 app = Flask(__name__, static_url_path='/static')
 app.debug = True
 
-<<<<<<< HEAD
-    # config = {
-    #     'user': 'root',
-    #     'password': 'root',
-    #     'host': 'db',
-    #     'port': '3306',
-    #     'database': 'quiz'
-    # }
-
-mysql = MySQL()
- 
-# MySQL configurations
-app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
-app.config['MYSQL_DATABASE_DB'] = 'quiz'
-app.config['MYSQL_DATABASE_HOST'] = 'db'
-mysql.init_app(app)
-
-conn = mysql.connect()
-
-cursor = conn.cursor()
+db = pymysql.connect("db", "root", "root", "quiz")
+cursor = db.cursor()
 
 
 @app.route('/' , methods = ['GET','POST'])
 def index() -> str:
     if request.method == "POST":
         if 'new_username' in request.form:
-            # Check if user exists in database
-            # If yes, then return "User already exists"
-            # If no, then add user into database and return "User added successfully"
-            
-            check_user = (
-                "SELECT user, COUNT(*) FROM user WHERE username = %s",
-        (request.form['new_username'])
-            )
-            cursor.execute(check_user)
-            row_count = result.rowcount
-            if row_count == 0:
-            insert_user = (
-                "INSERT INTO user (new_username) " "VALUES (%s)"
-            )
-            user_name = request.form['new_username']
-            cursor.execute(insert_user, user_name)
-            conn.commit()
+            print("###")
+            cursor.execute("SELECT * FROM user")
+            print(cursor.fetchall())
+            print("###")
+            #cursor.execute("SELECT * from user")
+            #data = cursor.fetchone()
+            #print(data)
+            return "HELLO"
         else:
-            console.log("USER NOT FOUND")
             # Start looking for a session for the user, username
+            print("JOINING GAME")
  
     else:
         return app.send_static_file('index.html')
